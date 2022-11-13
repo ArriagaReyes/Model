b409908ba406cbec89f12fb69b096496b44144eb
ArriagaReyes <hello@arriagareyes.me>
2022-11-12 20:56:54 -0500

    Added methods, accessible via the console, to post and get repositories

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,27 @@
 import '../styles.css';
+import axios from 'axios';
 import Experience from './experience/Experience';
 
 const experience = new Experience(document.getElementById('root'));
+
+window.post = (name) => {
+    axios.post('http://localhost:6969/api/repository', {
+        name
+    })
+    .then(({ data }) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+window.get = () => {
+    axios.get('http://localhost:6969/api/repository')
+    .then(({ data }) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
